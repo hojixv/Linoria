f1efc93d6e8b68d27186227e34de2cffc9c8c7a8
hojixv <153679337+hojixv@users.noreply.github.com>
2024-10-23 13:57:27 -0400

    SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'alysum'
+	SaveManager.Folder = 'Bind'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
